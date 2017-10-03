c59efa18b6b0bcb428ab6f6838c3c6decd02cbfe
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-03 16:48:57 +0100

    Find all TLSs which appear 63 times

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const fs = require('fs');
 const input = fs.readFileSync('resources/SampleText.txt', 'utf8');
 const tlsRegex = /[A-Za-z](?=([A-Za-z]{2}))/g
 
-let match, counter = 0;
+let match, tlsCounts = {};
 while ((match = tlsRegex.exec(input)) !== null) {
-    if (match[0].toLowerCase() + match[1].toLowerCase() === 'tra') {
-        counter++;
-    }
+    const tls = match[0].toLowerCase() + match[1].toLowerCase();
+    tlsCounts[tls] = (tlsCounts[tls] || 0) + 1;
 }
 
-console.log(`There are ${counter} instances of 'tra' in the text.`);
+console.log('The TLSs which appear 63 times are:');
+Object.keys(tlsCounts).filter(tls => tlsCounts[tls] === 63).forEach(tls =>
+    console.log(`  ${tls}`)
+);
